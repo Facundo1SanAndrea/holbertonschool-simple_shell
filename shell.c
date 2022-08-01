ada5e811e73cffbb9988ab3ca24334d43c7eab30
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-08-01 12:54:02 -0700

    Adding main body of the shell with some sapces

---
+++ shell.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	char *buffer = NULL;
+	int keepgetline, status;
+	char **buff_token;
+	size_t bufsize = 0;
+
+	while (true)
+	{
+		write(1, "#cisfun$ ", 8);
+
+		keepgetline = getline(&buffer, &bufsize, stdin);
+		
+		if (keepline == -1)
+			break;
+		
+		buff_token = _strtok(buffer);
+
+		if (buff_token[0] == "exit")
+		{
+			free(buff_token);
+			free(buffer);
+			exit(0);
+		}
+		if (buff_token[0] == "env")
+		{
+			_getenv();
+		}
+		
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("Error");
+		}
+		if (pid == 0)
+		{
+			execve(buff_token[0], buff_token, NULL);
+			free(buff_token);
+		}
+		else
+			wait(&status);
+			free(buff_token);
+	}
+	return (0);
+}
